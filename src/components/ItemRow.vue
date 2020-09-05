9fd5aaa4b8df28ad9d1917ff973e455f08b891a6
Your Name <you@example.com>
2020-09-05 23:28:02 +0630

    update item row

---
+++ src/components/ItemRow.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    
+        <tr >
+            <td> <img :alt=fruit.alt :src=fruit.image></td>
+            <td>{{fruit.name}}</td>
+            <td>{{fruit.price}} </td>
+            <!-- <td>{{total}} </td> -->
+            <td>  <button @click="$emit('remove-item', fruit)"> Remove</button>
+</td>
+            
+        </tr>
+        
+   
+</div>
+</template>
+
+<script>
+export default {
+    props:{
+        fruit:{
+            type: Object,
+            required:true
+        },
+        // total:{
+        //     type: Number,
+        //     required : true
+        // }
+       
+    }
+}
+</script>
